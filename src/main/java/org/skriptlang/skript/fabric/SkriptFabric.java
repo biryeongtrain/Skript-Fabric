d1296946c8937e3ad24032f4e1296b9a3bc739ba
biryeongtrain <biryeongtrain06@gmail.com>
2026-03-13 16:05:35 +0900

    fix(runtime): register skript brigadier command on init

--- src/main/java/org/skriptlang/skript/fabric/SkriptFabric.java
+++ src/main/java/org/skriptlang/skript/fabric/SkriptFabric.java
@@ -1,6 +1,7 @@
 package org.skriptlang.skript.fabric;
 
 import net.fabricmc.api.ModInitializer;
+import org.skriptlang.skript.fabric.runtime.SkriptCommandRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.skriptlang.skript.fabric.runtime.SkriptFabricBootstrap;
@@ -16,5 +17,6 @@ public final class SkriptFabric implements ModInitializer {
     public void onInitialize() {
         SkriptFabricBootstrap.bootstrap();
         SkriptRuntimeLifecycle.register(SkriptRuntimeServices.scriptService(), LOGGER);
+        SkriptCommandRegistration.register(SkriptRuntimeServices.scriptService());
     }
 }
